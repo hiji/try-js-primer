c85bcfad862268af188d8fad8755f42ce991ed33
Shinya Hijiri <shinya.hijiri@gmail.com>
2019-02-04 12:06:20 +0900

    第一部、データ型とリテラルまで

--- example/index.js
+++ example/index.js
@@ -1,1 +1,33 @@
 console.log(1); // => 1
+
+// typeof演算子でデータ型を調べられる
+console.log(typeof true); // => boolean
+
+// テンプレートリテラルだと変数展開できる
+const string  = "文字列"
+console.log(`これは${string}です`);
+
+// 複数行も書ける
+const multiString = `複数行の
+文字列を
+入れたい`;
+console.log(multiString);
+
+// オブジェクトリテラル。
+const object = {
+    "key": "value",
+    "123": "value"
+};
+console.log(object.key)
+console.log(object["key"]);
+// console.log(object.123); 数字始まりは識別子名ではNGなので、この記法では書けない
+console.log(object["123"]);
+
+// 配列リテラル
+const emptyArray = [];
+const array = [1, 2, 3];
+console.log(array[0]); // インデックスは0始まり
+
+// 正規表現
+const numberRegExp = /\d+/
+console.log(numberRegExp.test(123));
