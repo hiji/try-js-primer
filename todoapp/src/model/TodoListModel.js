00424583b3f1fba7b59ad9fb400a0079952cadb6
Shinya Hijiri <shinya.hijiri@gmail.com>
2019-02-07 00:54:33 +0900

    Event & Model

---
+++ todoapp/src/model/TodoListModel.js
@@ -0,0 +1,30 @@
+import { EventEmitter } from "../EventEmitter.js";
+
+export class TodoListModel extends EventEmitter {
+
+    constructor(items = []) {
+        super();
+        this.items = items;
+    }
+
+    get totalCount() {
+        return this.items.length;
+    }
+
+    getTodoItems() {
+        return this.items;
+    }
+
+    onChange(listener) {
+        this.addEvenetListener("change", listener);
+    }
+
+    emitChange() {
+        this.emit("change");
+    }
+
+    addTodo(todoItem) {
+        this.items.push(todoItem);
+        this.emitChange();
+    }
+}
